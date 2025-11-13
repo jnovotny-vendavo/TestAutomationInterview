be3cad58e1c12379650b4e81274730167abbbbd0
jnovotny <jnovotny@vendavo.com>
2025-11-13 12:27:23 +0100

    Initial commit

---
+++ TestAutomationInterview/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            var inventoryList = driver.FindElement(By.ClassName("inventory_list"));
+            Assert.That(inventoryList.Displayed, Is.True);
+        }
+    }
+}
